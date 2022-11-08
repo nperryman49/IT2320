fa19c7e599e2d8af7487c6a696480809864606a2
ManjulaChandirasekaran <35280800+ManjulaChandirasekaran@users.noreply.github.com>
2022-11-08 13:57:19 -0500

    Add files via upload

--- Lab 9-MiniProject2Part2/miniproject2part2.js
+++ Lab 9-MiniProject2Part2/miniproject2part2.js
@@ -12,13 +12,13 @@ for (i=0; i< players.length; i++)
     })
 }
 
-var computers=document.getElementsByClassName("col");
+/*var computers=document.getElementsByClassName("col");
 for (i=0; i< computers.length; i++)
 {
     computers[i].addEventListener ("click", function(){
         computerTurn(this);
     })
-}
+}*/
 
 var winningSquares =[[0,1,2],
 [3,4,5],
@@ -30,9 +30,9 @@ var winningSquares =[[0,1,2],
 [2,4,6]];
 for (i=0; i<winningSquares.length; i++)
 {
-    winningSquares[i].addEventListener ("click", function(){
+    /*winningSquares[i].addEventListener ("click", function(){
         determineWinner(this);
-    })
+    })*/
 }
 var resetbutton=document.getElementById("reset");
 resetbutton.addEventListener ("click", function(){
@@ -48,10 +48,14 @@ function userTurn(player)
 {
     //Add code here
     player.innerHTML="X";
-    computerTurn();
+    var winner = determineWinner("X");
+    if (winner == false){
+		computerTurn();
+	   winner = determineWinner("O");
+	}
     
 }
-function determineWinner ()
+function determineWinner (match)
 {
     var winningSquares =[[0,1,2],
     [3,4,5],
@@ -61,20 +65,26 @@ function determineWinner ()
     [2,5,8],
     [0,4,8],
     [2,4,6]];   
+    
+    var boxes=document.getElementsByClassName("col");
+
     for (i=0; i<winningSquares.length; i++){
 
         const winner=winningSquares[i];
         const square1=[winner[0]];
         const square2=[winner[1]];
         const square3=[winner[2]];
-
-        if (square1=="X" && square2=="X" && square3=="X")
+		console.log(square1);
+	console.log(square2);
+	console.log(square3);
+        if (boxes[square1].innerHTML==match && boxes[square2].innerHTML==match && boxes[square3].innerHTML==match)
           
         {
             var user=document.getElementById("message");
-            user.innerHTML="User is the winner!";
+            user.innerHTML=match + " is the winner!";
+			return true;
         }
-        else if (square1=="" || square2=="" || square3==""){
+        /*else if (square1=="" || square2=="" || square3==""){
             continue;
         }
         else 
@@ -82,8 +92,12 @@ function determineWinner ()
             var match=document.getElementById("status");
             match.innerHTML="Game Over";
             break;
-        }
-        }    
+        }*/
+
+		
+     } //end of for loop
+	
+	return false;   
         
 }
 
@@ -107,7 +121,7 @@ function computerTurn()
     }
 }
         
-function determineWinner()
+/*function determineWinner()
 {
     var winningSquares =[[0,1,2],
     [3,4,5],
@@ -143,7 +157,7 @@ function determineWinner()
                       
 }
    
-
+*/
 
 function ResetGameBoard()
 {
